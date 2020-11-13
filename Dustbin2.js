837976765fc84685c0d2a514c2f8750a92ae8c3d
chetan1234-git <71207837+chetan1234-git@users.noreply.github.com>
2020-11-13 10:31:32 +0530

    Add files via upload

---
+++ Dustbin2.js
@@ -0,0 +1,22 @@
+class Dustbin2 {
+    constructor(x, y, width, height) {
+      var options = {
+          isStatic: true,
+          restitution:0,
+          friction:0.8,
+          density:0.1
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){
+   rectMode(CENTER);
+
+      fill(255);
+
+      rect(this.body.position.x, this.body.position.y, this.width, this. height);
+    }
+  };
